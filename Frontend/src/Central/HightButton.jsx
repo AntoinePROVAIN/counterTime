d4ab239a7c58347edd830f8f1aa56384dc986dc9
AntoinePROVAIN <antoineprovain@gmail.com>
2024-11-25 00:08:34 +0100

    changement bouton monter et desc

--- Frontend/src/Central/HightButton.jsx
+++ Frontend/src/Central/HightButton.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
 import "../App.css";
 
 const HightButton = () => {
@@ -8,10 +7,12 @@ const HightButton = () => {
     <>
         <div className='h_butt'>
             <div className='b_haut'>
-                <a href='#entete-section' className='a_haut'>Haut de page</a>
+                <a href='#entete-section' className='a_haut'><img src='/chvron_h.png' className='img_b'></img></a>
+                <p className='txt_b'>Haut</p>
             </div>
             <div className='b_bas'>
-                <a href='#result-section' className='a_bas'>Bas de page</a>
+                <a href='#result-section' className='a_bas'><img src='/chvron_b.png' className='img_b'></img></a>
+                <p className='txt_b'>Bas</p>
             </div>
         </div>
     </>
